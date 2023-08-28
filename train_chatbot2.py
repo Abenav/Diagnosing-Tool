e2bb8d2efc1f126c1f9b4430239d05cece21ca72
Abenav <73566954+Abenav@users.noreply.github.com>
2023-08-28 14:49:08 +0530

    Create train_chatbot2.py

---
+++ train_chatbot2.py
@@ -0,0 +1,75 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+import tensorflow as tf
+import random
+
+# Load and process data from intents.json
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)  # Tokenize using 'punkt' tokenizer
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+classes = sorted(list(set(classes)))
+
+pickle.dump(words, open('words.pkl', 'wb'))  #Serialization
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Create training data
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+# Build the neural network model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Use the updated optimizer
+sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
+# Save the model
+model.save('chatbot_model.h5', hist)
+
+print("Model created")
